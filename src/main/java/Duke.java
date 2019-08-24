7cd2fd1c221eb3be930ec0caee3979090de54026
Carrie Ng <e0323852@u.nus.edu>
2019-08-25 01:40:08 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String ab = sc.nextLine();
+
+            if ("bye".equals(ab)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(ab);
+            }
+        }
     }
 }
