b9d694caa21676d2fbb6d8e71a666e05cb94fe37
Carrie Ng <e0323852@u.nus.edu>
2019-09-04 18:53:07 +0800

    level 9

--- src/main/java/inputFile.java
+++ src/main/java/inputFile.java
@@ -26,7 +26,7 @@ public class inputFile {
         BufferedWriter fw = new BufferedWriter(new FileWriter("C:\\Users\\65839\\duke\\data\\duke.txt", true));
         String[] token = output2.split(Pattern.quote(" "));
         String[] tokens = token[0].split(Pattern.quote("/"));
-        fw.write("D | 0 | " + output1 + " | " + tokens[0] + getsuffix(Integer.parseInt(tokens[0])) + " of " + getMonth(Integer.parseInt(tokens[1])) + " " + tokens[2] + ", " + getHours(token[1]) + ":" + token[1].charAt(2) + token[1].charAt(3) + convert12(token[1]));
+        fw.write("D | 0 | " + output1 + " | " + tokens[0] + Time.getsuffix(Integer.parseInt(tokens[0])) + " of " + Time.getMonth(Integer.parseInt(tokens[1])) + " " + tokens[2] + ", " + Time.getHours(token[1]) + ":" + token[1].charAt(2) + token[1].charAt(3) + Time.convert12(token[1]));
         fw.newLine();
         fw.close();
     }
@@ -35,7 +35,7 @@ public class inputFile {
         BufferedWriter fw = new BufferedWriter(new FileWriter("C:\\Users\\65839\\duke\\data\\duke.txt",true));
         String[] token = output2.split(Pattern.quote(" "));
         String[] tokens = token[0].split(Pattern.quote("/"));
-        fw.write("E | 0 | " + output1 + " | " + tokens[0] + getsuffix(Integer.parseInt(tokens[0])) + " of " + getMonth(Integer.parseInt(tokens[1])) + " " + tokens[2] + ", " + getHours(token[1]) + ":" + token[1].charAt(2) + token[1].charAt(3) + convert12(token[1]) );
+        fw.write("E | 0 | " + output1 + " | " + tokens[0] + Time.getsuffix(Integer.parseInt(tokens[0])) + " of " + Time.getMonth(Integer.parseInt(tokens[1])) + " " + tokens[2] + ", " + Time.getHours(token[1]) + ":" + token[1].charAt(2) + token[1].charAt(3) + Time.convert12(token[1]) );
         fw.newLine();
         fw.close();
     }
@@ -87,56 +87,5 @@ public class inputFile {
         print.close();
         currentFile.delete();
         temporaryFile.renameTo(new File("C:\\Users\\65839\\duke\\data\\duke.txt"));
-    }
-
-    static String getsuffix(int x) {
-        if (x >= 11 || x <= 13) {
-            return "th";
-        } else {
-            x %= 10;
-            if (x == 1) {
-                return "st";
-            } else if (x == 2) {
-                return "nd";
-            } else if (x == 3) {
-                return "rd";
-            } else {
-                return "th";
-            }
-        }
-    }
-
-    static String getMonth(int month) {
-        return new DateFormatSymbols().getMonths()[month - 1];
-    }
-
-    static int getHours(String str) {
-        int h1 = (int) str.charAt(0) - '0';
-        int h2 = (int) str.charAt(1) - '0';
-        int hh = (h1 * 10) + h2;
-
-        hh %= 12;
-
-        if (hh == 0) {
-            return 12;
-        } else {
-            return hh;
-        }
-    }
-
-    static String convert12(String str) {
-        int h1 = (int) str.charAt(0) - '0';
-        int h2 = (int) str.charAt(1) - '0';
-        int hh = (h1 * 10) + h2;
-
-        if (hh < 12) {
-            return "am";
-        } else {
-            return "pm";
-        }
-    }
-
-    public void closeFile() {
-        fm.close();
     }
 }
